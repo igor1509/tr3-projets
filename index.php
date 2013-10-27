a259a62978a68893445f32f1725869467b1e1de0
GD15 <guillaume.demarez@yahoo.fr>
2013-10-27 14:29:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style.css"/>
+        <title></title>
+    </head>
+    <body>
+        <header>
+        <img src="images/logo.png" id="logo" alt="logo du site"/><br/>
+        <span id="intro">Bienvenue chez AquaService</span>
+        </header>
+        <?php
+        
+        ?>
+    </body>
+</html>
